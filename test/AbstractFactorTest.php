d91d9df6d6393922e6298f8d882339591c1a09f7
MohamedSalamaMouse <slamtm608@gmail.com>
2023-04-28 20:05:39 +0200

    structural_behavioral

--- test/AbstractFactorTest.php
+++ test/AbstractFactorTest.php
@@ -8,11 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 class AbstractFactorTest extends TestCase
 {
-    public function testcanCreateBMW(){
-        $carFactory=new CarAbstractFactory(200000);
-      $mycar=  $carFactory->CreateBMWCar();
-      $this->assertInstanceOf(BMWCar::class,$mycar);
-    }
-
-
+  public function testcanCreateBMW()
+  {
+    $carFactory = new CarAbstractFactory(200000);
+    $mycar =  $carFactory->CreateBMWCar();
+    $this->assertInstanceOf(BMWCar::class, $mycar);
+    #asserttrue(true)
+  }
 }
